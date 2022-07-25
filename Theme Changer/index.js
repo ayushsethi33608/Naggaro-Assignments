8d2ad6d8da003af1f06991034dd7cbaffff35cba
ayushsethi33608 <ayushsethi33608@gmail.com>
2022-07-25 18:07:40 +0530

    Assignment

---
+++ Theme Changer/index.js
@@ -0,0 +1,40 @@
+function black(){
+    var x = document.getElementsByTagName("BODY")[0];
+    x.style.background='black';
+    var y=document.getElementById('nav');
+    y.style.background='lightblue';
+    var z=document.getElementById('text');
+    z.style.border='5px solid white';
+    z.style.color='white';
+    z.style.background='black';
+}
+function blue(){
+    var x = document.getElementsByTagName("BODY")[0];
+    x.style.background='blue';
+    var y=document.getElementById('nav');
+    y.style.background='lightblue';
+    var z=document.getElementById('text');
+    z.style.border='5px solid white';
+    z.style.color='white';
+    z.style.background='blue';
+}
+function green(){
+    var x = document.getElementsByTagName("BODY")[0];
+    x.style.background='green';
+    var y=document.getElementById('nav');
+    y.style.background='lightblue';
+    var z=document.getElementById('text');
+    z.style.border='5px solid black';
+    z.style.color='black';
+    z.style.background='green'
+}
+function yellow(){
+    var x = document.getElementsByTagName("BODY")[0];
+    x.style.background='yellow';
+    var y=document.getElementById('nav');
+    y.style.background='lightblue';
+    var z=document.getElementById('text');
+    z.style.border='5px solid black';
+    z.style.color='black';
+    z.style.background='yellow';
+}
